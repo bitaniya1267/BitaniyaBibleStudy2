2c2e66de0518127b85a43fcc02d7d92461fced17
bitaniyabiblestudy <bitaniyatemesgen49@gmail.com>
2026-09-16 17:02:25 +0300

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/BitaniyaBibleStudy/',
+  base: '/BitaniyaBibleStudy2/',
   plugins: [react()],
 })
